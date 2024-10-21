5dfcbb93d9720f8b42e5b521d3f6148f3d9c481e
Andazio00 <daeniseuallen@gmail.com>
2024-10-21 19:53:26 +0900

    [level 0] Title: 문자열 곱하기, Time: 2.41 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        int i ;
+        
+        for(i = 1 ; i<= k ; i++){
+            answer += my_string;
+        }
+        
+        
+        return answer;
+    }
+}
